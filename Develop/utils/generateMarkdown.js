c9e73e86635dd6b49bda7b1c80e9e161e7e12140
Luis <luisriveraphi@gmail.com>
2024-02-25 19:10:42 -0500

    added license functions, also added markdown function for dynamic population of sections

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,73 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "MIT") {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]';
+  } else if (license === 'APACHE 2.0') {
+    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)'
+  } else if (license === 'GPL 3.0') {
+    return '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)]';
+  } else {
+    return "";
+  };
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return '(https://opensource.org/licenses/MIT)';
+  } else if (license === 'APACHE 2.0') {
+    return console.log('(https://opensource.org/licenses/Apache-2.0)');
+  } else if (license === 'GPL 3.0') {
+    return 'https://www.gnu.org/licenses/agpl-3.0)';
+  } else {
+    return "";
+  };
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseText = '';
+  switch (license) {
+    case 'MIT':
+      licenseText = `## License 
+      This project is licensed under the MIT License - for more info click here - (https://opensource.org/licenses/MIT)`
+      break;
+    case 'APACHE 2.0':
+      licenseText = `## License
+      This project is licensed uner the APACHE 2.0 License - for more info click here - (https://opensource.org/licenses/Apache-2.0)`
+    case 'GPL 3.0':
+      licenseText = `## License
+      This project is licensed uner the GPLv3 License - for more info click here - https://www.gnu.org/licenses/agpl-3.0)`
+    default:
+      break;
+  };
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## ${data.description}
 
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## License
+  ${data.license}
+  ## Tests
+  ${data.tests}
+  ## Questions
+  Contact me at [${data.username}](https://github.com/${data.username}) or email at ${data.email}.
 `;
 }
 
